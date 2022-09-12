e904fedd0ef4b9730514327143f3700a4afd4ccb
Marina <marishka-2008-91@mail.ru>
2022-09-12 11:20:14 +0300

    3

---
+++ Z3/Program.cs
@@ -0,0 +1,21 @@
+﻿/*  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33] */
+
+int[] GetArray(){
+    int[] result = new int[8];
+    for(int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(1, 10);
+    }
+    return result;
+}
+void PrintArray(int[]arr) {
+    for(int i = 0; i < arr.Length; i++)
+    {
+    System.Console.Write(arr[i]);
+    }
+}
+PrintArray(GetArray());
